c86c946aa1161eb34fb2ae05848406c61b9b1c9b
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-23 15:09:44 -0400

    Basic Authentication

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -18,7 +18,7 @@ partnerRouter.route('/')
     .post((req, res, next) => {
         Partner.create(req.body)
             .then(partner => {
-                console.log('Campsite Created ', partner);
+                console.log('Partner Created ', partner);
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 res.json(partner);
