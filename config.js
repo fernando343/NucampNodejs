bcf02a2962f5c4bfec6ad1de3ffd4480625a5c86
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-30 20:25:30 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  'facebook': {
+    clientId: '1146082672558658',
+    clientSecret: '4c531ab52dc4e988041c417dd67f65ab'
+}
 };
